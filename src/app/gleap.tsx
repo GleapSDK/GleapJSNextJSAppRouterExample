d3cd7bca28bee8b64fab6359a4c33560bf9d6a16
LucaB01 <luca@boehlerbrothers.com>
2025-11-17 14:51:49 +0100

    Add api key input

--- src/app/gleap.tsx
+++ src/app/gleap.tsx
@@ -1,12 +1,18 @@
 'use client';
 
 import { FC, ReactNode, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
 import Gleap from 'gleap';
 
 export const GleapInit: FC<{ children?: ReactNode }> = ({ children }) => {
+  const searchParams = useSearchParams();
+
   useEffect(() => {
-    Gleap.initialize("X5C0grjFCjUMbZKi131MjZLaGRwg2iKH");
-  });
+    const widgetKey = searchParams.get('widgetKey');
+    if (widgetKey) {
+      Gleap.initialize(widgetKey);
+    }
+  }, [searchParams]);
 
   return <>{children}</>;
 };
